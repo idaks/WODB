b4fdabde3edfdb5ec4db36c0811680a2527302c3
Bertram Ludaescher <ludaesch@illinois.edu>
2020-07-23 19:28:53 -0500

    added automorphism code

--- automorphisms/property-ID-V/run-all.sh
+++ automorphisms/property-ID-V/run-all.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+echo "*** Permuting Example 3 to obtain Example 4 ***" 
 source permute.sh
 
 for f  in ex*.lp4
 do
-    echo "*** Processing file $f ***"
-    clingo -n0 $f unique.lp4
+    echo "*** Evaluating UNIQUE QUERY on DB $f ***"
+    clingo -n0 unique.lp4 $f 
+    echo
+done
+
+
+for f  in ex*.lp4
+do
+    echo "*** Evaluating AUTOMORPHISM on DB $f ***"
+    clingo -n0 automorphism.lp4 $f 
     echo
 done
 
