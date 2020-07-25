34d6d8fa9f1d5d8794a11945a2929bc438e80b7e
Bertram Ludaescher <ludaesch@illinois.edu>
2020-07-25 06:41:08 -0500

    updated choice rules (predicate names) and re-ran experiment on old MBP 15''

--- automorphisms/choice-experiment/run-choice-experiment.sh
+++ automorphisms/choice-experiment/run-choice-experiment.sh
@@ -3,7 +3,8 @@
 clingo -n0 -V0 -s1 choice-disjunction.pl | grep ":" > choice-disjunction.log
 clingo -n0 -V0 -s1 choice-curly.pl | grep ":" > choice-curly.log
 
-diff choice-disjunction.log choice-curly.log
+diff choice-disjunction.log choice-curly.log > choice.diff
+
 
 
 
